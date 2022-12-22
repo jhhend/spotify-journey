00439c810ec3ce165d0bbdd1b1fc7cc65e77f71e
jhhend <121206636+jhhend@users.noreply.github.com>
2022-12-21 17:57:24 -0800

    Final project archive

---
+++ src/API_Interface/API_Interface.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+
+const AxiosConfigured = () => {
+    // Indicate to the API that all requests for this app are AJAX
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    // Set the baseURL for all requests to the API domain instead of the current domain
+    // axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+    // Uncomment the following line if the API server runs on the same host as your UI server.
+    axios.defaults.baseURL = `http://localhost:8443/api/v1`;
+
+    // Allow the browser to send cookies to the API domain (which include auth_token)
+    axios.defaults.withCredentials = true;
+
+    //axios.defaults.headers.common['X-CSRF-TOKEN'] = csrf_token;
+    return axios;
+};
+
+const axiosAgent = AxiosConfigured();
+
+export default class APIInterface {
+
+    async getUserInfo(username) {
+        console.log('We have called getUserInfo')
+        return axiosAgent.get(`login/${username}`)
+            .then(userInfo => userInfo.data)
+            .catch(error => (
+                {
+                    error,
+                    user: undefined
+                 }));
+    }
+
+    async getUserPlaylists(username) {
+        console.log('We have called getUserPlaylists');
+        return axiosAgent.get(`playlist/${username}`)
+            .then(info => info.data)
+            .catch(error => ({
+                error,
+                playlists : undefined
+            }));
+    }
+
+    async getSongsFromPlaylist(playlist) {
+        console.log('We have called getSongsFromPlaylist');
+        return axiosAgent.get(`song/${playlist}`)
+            .then(info => info.data)
+            .catch(error => ({
+                error,
+                songs : undefined
+            }))
+    }
+
+    async getSongsFromPlaylistWithGenres(playlist) {
+        console.log('We have called getSongsFromPlaylistWithGenres');
+        return axiosAgent.get(`song/withgenre/${playlist}`)
+            .then(info => info.data)
+            .catch(error => ({
+                error,
+                songs : undefined
+            }))
+    }
+
+    async register(username, password, firstName, lastName) {
+        return axiosAgent.post(`/login/register`);
+    }
+
+    async getUser(username) {
+        return axiosAgent.get(`/${username}`);
+    }
+
+    // async addPlaylist()
+
+}
+
